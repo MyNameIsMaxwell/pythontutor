f9bfee1e8c3f74bd84a754b1b4a9f3f4bef32e54
needforce <makslyubchuk@mail.ru>
2018-05-26 21:32:37 +0300

    8.py

--- 8.py
+++ 8.py
@@ -2,15 +2,17 @@ desk1_hor = int(input())
 desk1_ver = int(input())
 desk2_hor = int(input())
 desk2_ver = int(input())
-b=desk1_ver==desk2_ver
-a=desk1_hor==desk2_hor
-if (a+1 and b) or (a-1 and b):
+step_up=desk1_hor==desk2_hor+1 and desk1_ver==desk2_ver
+step_down=desk1_hor==desk2_hor-1 and desk1_ver==desk2_ver
+step_right=desk1_hor==desk2_hor and desk1_ver==desk2_ver+1
+step_left=desk1_hor==desk2_hor and desk1_ver==desk2_ver-1
+step_diag_up_left=desk1_hor==desk2_hor+1 and desk1_ver==desk2_ver-1
+step_diag_up_right=desk1_hor==desk2_hor+1 and desk1_ver==desk2_ver+1
+step_diag_down_left=desk1_hor==desk2_hor-1 and desk1_ver==desk2_ver-1
+step_diag_down_right=desk1_hor==desk2_hor-1 and desk1_ver==desk2_ver+1
+if step_up or step_down or step_right or step_left:
     print("YES")
-elif (a and b+1) or (a and b-1):
-    print("YES")
-elif (a+1 and b+1)or(a-1 and b-1):
-    print("Yes")
-elif (a-1 and b+1)or(a+1 and b-1):
+elif step_diag_up_left or step_diag_up_right or step_diag_down_left or step_diag_down_right :
     print("Yes")
 else:
-    print("NO") #да епт
+    print("NO")
