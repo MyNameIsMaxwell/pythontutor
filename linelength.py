1e782817460fcab367d30d07c7bc12a7eea8e66d
needforce <makslyubchuk@mail.ru>
2018-07-17 20:59:49 +0300

    functions

--- linelength.py
+++ linelength.py
@@ -1,8 +1,8 @@
 def distance(x1, y1, x2, y2):
     a=(x2-x1)+(y1-y1)
     b=(x2-x2)+(y2-y1)
-    c=(a**2+b**2)**(1/2)
-    return c
+    GipotenusaLength=(a**2+b**2)**(1/2)
+    return GipotenusaLength
     
 x1, y1 ,x2, y2=input().split()
 x1,y1,x2,y2=[int(x1),int(y1),int(x2),int(y2)]
